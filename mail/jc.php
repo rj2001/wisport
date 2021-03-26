f1d00b66fd48117d742bb6ff55078ac953e3d005
rj2001 <rgojr2001@yahoo.com>
2021-03-25 21:21:48 -0500

    TS mail issue

--- mail/jc.php
+++ mail/jc.php
@@ -1,5 +1,5 @@
 <?php 
-$to="support@rn53themes.net";
+$to="rgojr2001@yahoo.com";
 $subject="Website Enquiry";
 $data=$_POST['jname']."<br>";
 $data.=$_POST['jmail']."<br>";
@@ -14,8 +14,8 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: <rj_ochmann@wi-sport.com>' . "\r\n";
-$headers .= 'Cc: rgojr2001@yahoo.com' . "\r\n";
+$headers .= 'From: <admin@website.com>' . "\r\n";
+$headers .= 'Cc: rn53themes@gmail.com' . "\r\n";
 
 mail($to,$subject,$data,$headers);
 ?>
